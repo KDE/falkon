QupZilla/qupzilla

Use window._qupzilla_external property to check if web channel is connected
Instead of checking for window.external as that is now defined with
at least QtWebEngine 5.10.

src/lib/tools/scripts.cpp
@@ -31,12 +31,13 @@ QString Scripts::setupWebChannel()
                            "    if (window.external) {"
                            "        var event = document.createEvent('Event');"
                            "        event.initEvent('_qupzilla_external_created', true, true);"
+                           "        window._qupzilla_external = true;"
                            "        document.dispatchEvent(event);"
                            "    }"
                            "}"
                            ""
                            "if (self !== top) {"
-                           "    if (top.external)"
+                           "    if (top._qupzilla_external)"
                            "        registerExternal(top.external);"
                            "    else"
                            "        top.document.addEventListener('_qupzilla_external_created', function() {"

KDE/falkon

src/lib/tools/scripts.cpp
@@ -31,12 +31,13 @@ QString Scripts::setupWebChannel()
                            "    if (window.external) {"
                            "        var event = document.createEvent('Event');"
                            "        event.initEvent('_falkon_external_created', true, true);"
+                           "        window._falkon_external = true;"
                            "        document.dispatchEvent(event);"
                            "    }"
                            "}"
                            ""
                            "if (self !== top) {"
-                           "    if (top.external)"
+                           "    if (top._falkon_external)"
                            "        registerExternal(top.external);"
                            "    else"
                            "        top.document.addEventListener('_falkon_external_created', function() {"
8a6547b39c48337518026a4934352f9627029d0c
David Rosca <nowrep@gmail.com>
2018-01-03 16:38:45 +0100

    Use window._qupzilla_external property to check if web channel is connected
    Instead of checking for window.external as that is now defined with
    at least QtWebEngine 5.10.

--- src/plugins/GreaseMonkey/data/values.js
+++ src/plugins/GreaseMonkey/data/values.js
@@ -29,17 +29,17 @@ function GM_setValue(aKey, aVal) {
 
 // GreaseMonkey 4.0 support
 var asyncCall = (func) => {
-    if (window.external.extra) {
+    if (window._falkon_external) {
         func();
     } else {
-        document.addEventListener("_qupzilla_external_created", func);
+        document.addEventListener("_falkon_external_created", func);
     }
 };
 
 GM.deleteValue = function(name) {
     return new Promise((resolve, reject) => {
         asyncCall(() => {
-            window.external.extra.greasemonkey.deleteValue("%1", name, (res) => {
+            external.extra.greasemonkey.deleteValue("%1", name, (res) => {
                 if (res) {
                     resolve();
                 } else {
@@ -53,7 +53,7 @@ GM.deleteValue = function(name) {
 GM.getValue = function(name, value) {
     return new Promise((resolve) => {
         asyncCall(() => {
-            window.external.extra.greasemonkey.getValue("%1", name, value, resolve);
+            external.extra.greasemonkey.getValue("%1", name, value, resolve);
         });
     });
 };
@@ -61,7 +61,7 @@ GM.getValue = function(name, value) {
 GM.setValue = function(name, value) {
     return new Promise((resolve, reject) => {
         asyncCall(() => {
-            window.external.extra.greasemonkey.setValue("%1", name, value, (res) => {
+            external.extra.greasemonkey.setValue("%1", name, value, (res) => {
                 if (res) {
                     resolve();
                 } else {
@@ -75,7 +75,7 @@ GM.setValue = function(name, value) {
 GM.listValues = function() {
     return new Promise((resolve) => {
         asyncCall(() => {
-            window.external.extra.greasemonkey.listValues("%1", resolve);
+            external.extra.greasemonkey.listValues("%1", resolve);
         });
     });
 };
