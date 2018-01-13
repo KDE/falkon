QupZilla/qupzilla

Add support for window.print()
Closes #2509

src/lib/app/mainapplication.cpp
@@ -1138,13 +1138,13 @@ void MainApplication::setupUserScripts()
     script2.setSourceCode(Scripts::setupWebChannel(script2.worldId()));
     m_webProfile->scripts()->insert(script2);
 
-    // window.external support
+    // document.window object addons
     QWebEngineScript script3;
-    script3.setName(QSL("_qupzilla_window_external"));
+    script3.setName(QSL("_qupzilla_window_object"));
     script3.setInjectionPoint(QWebEngineScript::DocumentCreation);
     script3.setWorldId(WebPage::UnsafeJsWorld);
     script3.setRunsOnSubFrames(true);
-    script3.setSourceCode(Scripts::setupWindowExternal());
+    script3.setSourceCode(Scripts::setupWindowObject());
     m_webProfile->scripts()->insert(script3);
 }
 

src/lib/tools/scripts.cpp
@@ -143,7 +143,7 @@ QString Scripts::setupFormObserver()
     return source;
 }
 
-QString Scripts::setupWindowExternal()
+QString Scripts::setupWindowObject()
 {
     QString source = QL1S("(function() {"
                           "var external = {};"
@@ -155,6 +155,9 @@ QString Scripts::setupWindowExternal()
                           "    return false;"
                           "};"
                           "window.external = external;"
+                          "window.print = function() {"
+                          "    window.location = 'qupzilla:PrintPage';"
+                          "};"
                           "})()");
 
     return source;

src/lib/webengine/webpage.cpp
@@ -380,10 +380,15 @@ bool WebPage::acceptNavigationRequest(const QUrl &url, QWebEnginePage::Navigatio
     if (!mApp->plugins()->acceptNavigationRequest(this, url, type, isMainFrame))
         return false;
 
-    if (url.scheme() == QL1S("qupzilla") && url.path() == QL1S("AddSearchProvider")) {
-        QUrlQuery query(url);
-        mApp->searchEnginesManager()->addEngine(QUrl(query.queryItemValue(QSL("url"))));
-        return false;
+    if (url.scheme() == QL1S("qupzilla")) {
+        if (url.path() == QL1S("AddSearchProvider")) {
+            QUrlQuery query(url);
+            mApp->searchEnginesManager()->addEngine(QUrl(query.queryItemValue(QSL("url"))));
+            return false;
+        } else if (url.path() == QL1S("PrintPage")) {
+            emit printRequested();
+            return false;
+        }
     }
 
     const bool result = QWebEnginePage::acceptNavigationRequest(url, type, isMainFrame);

KDE/falkon

src/lib/app/mainapplication.cpp
@@ -1136,13 +1136,13 @@ void MainApplication::setupUserScripts()
     script2.setSourceCode(Scripts::setupWebChannel(script2.worldId()));
     m_webProfile->scripts()->insert(script2);
 
-    // window.external support
+    // document.window object addons
     QWebEngineScript script3;
-    script3.setName(QSL("_falkon_window_external"));
+    script3.setName(QSL("_falkon_window_object"));
     script3.setInjectionPoint(QWebEngineScript::DocumentCreation);
     script3.setWorldId(WebPage::UnsafeJsWorld);
     script3.setRunsOnSubFrames(true);
-    script3.setSourceCode(Scripts::setupWindowExternal());
+    script3.setSourceCode(Scripts::setupWindowObject());
     m_webProfile->scripts()->insert(script3);
 }
 

src/lib/tools/scripts.cpp
@@ -143,7 +143,7 @@ QString Scripts::setupFormObserver()
     return source;
 }
 
-QString Scripts::setupWindowExternal()
+QString Scripts::setupWindowObject()
 {
     QString source = QL1S("(function() {"
                           "var external = {};"
@@ -155,6 +155,9 @@ QString Scripts::setupWindowExternal()
                           "    return false;"
                           "};"
                           "window.external = external;"
+                          "window.print = function() {"
+                          "    window.location = 'falkon:PrintPage';"
+                          "};"
                           "})()");
 
     return source;

src/lib/webengine/webpage.cpp
@@ -380,10 +380,15 @@ bool WebPage::acceptNavigationRequest(const QUrl &url, QWebEnginePage::Navigatio
     if (!mApp->plugins()->acceptNavigationRequest(this, url, type, isMainFrame))
         return false;
 
-    if (url.scheme() == QL1S("falkon") && url.path() == QL1S("AddSearchProvider")) {
-        QUrlQuery query(url);
-        mApp->searchEnginesManager()->addEngine(QUrl(query.queryItemValue(QSL("url"))));
-        return false;
+    if (url.scheme() == QL1S("falkon")) {
+        if (url.path() == QL1S("AddSearchProvider")) {
+            QUrlQuery query(url);
+            mApp->searchEnginesManager()->addEngine(QUrl(query.queryItemValue(QSL("url"))));
+            return false;
+        } else if (url.path() == QL1S("PrintPage")) {
+            emit printRequested();
+            return false;
+        }
     }
 
     const bool result = QWebEnginePage::acceptNavigationRequest(url, type, isMainFrame);