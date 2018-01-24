61c24615e6ac76105c6e967e59ef99528d85d392
David Rosca <nowrep@gmail.com>
2018-01-24 15:27:14 +0100

    Remove git_hooks

--- git_hooks/hooks-install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#
-# Install hooks into .git/hooks directory
-#
-
-cp pre-commit ../.git/hooks/pre-commit
-cp post-commit ../.git/hooks/post-commit
-cp post-checkout ../.git/hooks/post-checkout
-cp post-merge ../.git/hooks/post-merge
