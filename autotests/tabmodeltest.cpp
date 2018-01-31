59dbfc1f48edbd18246053b5be0a55745f7e85ee
David Rosca <nowrep@gmail.com>
2018-01-31 20:21:00 +0100

    TabTreeModel: Reset model when source resets

--- autotests/tabmodeltest.cpp
+++ autotests/tabmodeltest.cpp
@@ -116,8 +116,6 @@ void TabModelTest::dataTest()
 
     w->tabWidget()->addView(QUrl("http://test.com"));
 
-    WebTab *tab1 = w->weView(1)->webTab();
-
     QTest::qWait(10);
     delete w;
 }
@@ -217,6 +215,23 @@ void TabModelTest::treeModelTest()
 
     QTest::qWait(10);
     delete w;
+}
+
+void TabModelTest::resetTreeModelTest()
+{
+    BrowserWindow *w = mApp->createWindow(Qz::BW_NewWindow);
+
+    TabModel sourceModel(w);
+    TabTreeModel model;
+    model.setSourceModel(&sourceModel);
+    ModelTest modelTest(&model);
+
+    QTRY_COMPARE(model.rowCount(QModelIndex()), 1);
+
+    QTest::qWait(1);
+    delete w;
+
+    QCOMPARE(model.rowCount(QModelIndex()), 0);
 }
 
 FALKONTEST_MAIN(TabModelTest)
