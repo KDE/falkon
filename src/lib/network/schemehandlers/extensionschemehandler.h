QupZilla/qupzilla

NetworkManager: Add support for "extension://" scheme

src/lib/network/schemehandlers/extensionschemehandler.h
@@ -1,0 +1,47 @@
+/* ============================================================
+* QupZilla - Qt web browser
+* Copyright (C) 2018 David Rosca <nowrep@gmail.com>
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program.  If not, see <http://www.gnu.org/licenses/>.
+* ============================================================ */
+#pragma once
+
+#include <QWebEngineUrlSchemeHandler>
+
+#include "qzcommon.h"
+
+class ExtensionSchemeHandler : public QObject
+{
+public:
+    explicit ExtensionSchemeHandler(QObject *parent = nullptr) : QObject(parent) { }
+
+    virtual void requestStarted(QWebEngineUrlRequestJob *job) = 0;
+
+protected:
+    void setReply(QWebEngineUrlRequestJob *job, const QByteArray &contentType, const QByteArray &content);
+};
+
+class QUPZILLA_EXPORT ExtensionSchemeManager : public QWebEngineUrlSchemeHandler
+{
+public:
+    explicit ExtensionSchemeManager(QObject *parent = nullptr);
+
+    void requestStarted(QWebEngineUrlRequestJob *job) override;
+
+    void registerHandler(const QString &name, ExtensionSchemeHandler *handler);
+    void unregisterHandler(const QString &name);
+
+private:
+    QHash<QString, ExtensionSchemeHandler*> m_handlers;
+};

src/lib/other/siteinfo.cpp
@@ -1,6 +1,6 @@
 /* ============================================================
-* QupZilla - WebKit based browser
-* Copyright (C) 2010-2016 David Rosca <nowrep@gmail.com>
+* QupZilla - Qt web browser
+* Copyright (C) 2010-2018 David Rosca <nowrep@gmail.com>
 *
 * This program is free software: you can redistribute it and/or modify
 * it under the terms of the GNU General Public License as published by
@@ -139,7 +139,7 @@ bool SiteInfo::canShowSiteInfo(const QUrl &url)
     if (LocationBar::convertUrlToText(url).isEmpty())
         return false;
 
-    if (url.scheme() == QL1S("qupzilla") || url.scheme() == QL1S("view-source"))
+    if (url.scheme() == QL1S("qupzilla") || url.scheme() == QL1S("view-source") || url.scheme() == QL1S("extension"))
         return false;
 
     return true;

src/lib/webengine/webpage.cpp
@@ -377,7 +377,7 @@ void WebPage::setupWebChannelForUrl(const QUrl &url)
         channel = new QWebChannel(this);
         ExternalJsObject::setupWebChannel(channel, this);
     }
-    if (url.scheme() == QL1S("qupzilla")) {
+    if (url.scheme() == QL1S("qupzilla") || url.scheme() == QL1S("extension")) {
         setWebChannel(channel, UnsafeJsWorld);
     } else {
         setWebChannel(channel, SafeJsWorld);

KDE/falkon

src/lib/network/schemehandlers/extensionschemehandler.h
@@ -1,0 +1,47 @@
+/* ============================================================
+* Falkon - Qt web browser
+* Copyright (C) 2018 David Rosca <nowrep@gmail.com>
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program.  If not, see <http://www.gnu.org/licenses/>.
+* ============================================================ */
+#pragma once
+
+#include <QWebEngineUrlSchemeHandler>
+
+#include "qzcommon.h"
+
+class FALKON_EXPORT ExtensionSchemeHandler : public QObject
+{
+public:
+    explicit ExtensionSchemeHandler(QObject *parent = nullptr) : QObject(parent) { }
+
+    virtual void requestStarted(QWebEngineUrlRequestJob *job) = 0;
+
+protected:
+    void setReply(QWebEngineUrlRequestJob *job, const QByteArray &contentType, const QByteArray &content);
+};
+
+class FALKON_EXPORT ExtensionSchemeManager : public QWebEngineUrlSchemeHandler
+{
+public:
+    explicit ExtensionSchemeManager(QObject *parent = nullptr);
+
+    void requestStarted(QWebEngineUrlRequestJob *job) override;
+
+    void registerHandler(const QString &name, ExtensionSchemeHandler *handler);
+    void unregisterHandler(const QString &name);
+
+private:
+    QHash<QString, ExtensionSchemeHandler*> m_handlers;
+};

src/lib/other/siteinfo.cpp
@@ -1,6 +1,6 @@
 /* ============================================================
 * Falkon - Qt web browser
-* Copyright (C) 2010-2016 David Rosca <nowrep@gmail.com>
+* Copyright (C) 2010-2018 David Rosca <nowrep@gmail.com>
 *
 * This program is free software: you can redistribute it and/or modify
 * it under the terms of the GNU General Public License as published by
@@ -139,7 +139,7 @@ bool SiteInfo::canShowSiteInfo(const QUrl &url)
     if (LocationBar::convertUrlToText(url).isEmpty())
         return false;
 
-    if (url.scheme() == QL1S("falkon") || url.scheme() == QL1S("view-source"))
+    if (url.scheme() == QL1S("falkon") || url.scheme() == QL1S("view-source") || url.scheme() == QL1S("extension"))
         return false;
 
     return true;

src/lib/webengine/webpage.cpp
@@ -377,7 +377,7 @@ void WebPage::setupWebChannelForUrl(const QUrl &url)
         channel = new QWebChannel(this);
         ExternalJsObject::setupWebChannel(channel, this);
     }
-    if (url.scheme() == QL1S("falkon")) {
+    if (url.scheme() == QL1S("falkon") || url.scheme() == QL1S("extension")) {
         setWebChannel(channel, UnsafeJsWorld);
     } else {
         setWebChannel(channel, SafeJsWorld);