6dff236e0795e52800daac15929b5421308c9f16
David Rosca <nowrep@gmail.com>
2018-02-25 22:26:37 +0100

    Fix exporting QtSingleApplication

--- src/lib/3rdparty/qtsingleapplication/qtsingleapplication.h
+++ src/lib/3rdparty/qtsingleapplication/qtsingleapplication.h
@@ -49,25 +49,11 @@
 
 #include <QApplication>
 
+#include "qzcommon.h"
+
 class QtLocalPeer;
 
-#if defined(Q_OS_WIN)
-#  if !defined(QT_QTSINGLEAPPLICATION_EXPORT) && !defined(QT_QTSINGLEAPPLICATION_IMPORT)
-#    define QT_QTSINGLEAPPLICATION_EXPORT
-#  elif defined(QT_QTSINGLEAPPLICATION_IMPORT)
-#    if defined(QT_QTSINGLEAPPLICATION_EXPORT)
-#      undef QT_QTSINGLEAPPLICATION_EXPORT
-#    endif
-#    define QT_QTSINGLEAPPLICATION_EXPORT __declspec(dllimport)
-#  elif defined(QT_QTSINGLEAPPLICATION_EXPORT)
-#    undef QT_QTSINGLEAPPLICATION_EXPORT
-#    define QT_QTSINGLEAPPLICATION_EXPORT __declspec(dllexport)
-#  endif
-#else
-#  define QT_QTSINGLEAPPLICATION_EXPORT
-#endif
-
-class QT_QTSINGLEAPPLICATION_EXPORT QtSingleApplication : public QApplication
+class FALKON_EXPORT QtSingleApplication : public QApplication
 {
     Q_OBJECT
 
