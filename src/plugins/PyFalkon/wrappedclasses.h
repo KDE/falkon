3f1d37ab69be28ad5e1bcd5b2a16f6ef40fc6188
David Rosca <nowrep@gmail.com>
2018-02-26 08:35:09 +0100

    PyFalkon: Wrap more classes

--- src/plugins/PyFalkon/wrappedclasses.h
+++ src/plugins/PyFalkon/wrappedclasses.h
@@ -16,6 +16,34 @@
 * along with this program.  If not, see <http://www.gnu.org/licenses/>.
 * ============================================================ */
 
-#include "webview.h"
-#include "desktopfile.h"
+// 3rdparty
+#include "lineedit.h"
+#include "squeezelabelv1.h"
+#include "squeezelabelv2.h"
+#include "qtsingleapplication/qtsingleapplication.h"
+
+// app
+#include "mainapplication.h"
+
+// notifications
+#include "desktopnotificationsfactory.h"
+
+// plugins
+#include "pluginproxy.h"
 #include "plugininterface.h"
+
+// popupwindow
+#include "popupwebview.h"
+
+// tools
+#include "desktopfile.h"
+
+// webengine
+#include "webpage.h"
+#include "webview.h"
+#include "loadrequest.h"
+#include "javascript/externaljsobject.h"
+
+// webtab
+#include "webtab.h"
+#include "tabbedwebview.h"
