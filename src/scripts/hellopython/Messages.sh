3c8fcd5f70a0c2dbc7bfd0c264380d122b6dcfd3
David Rosca <nowrep@gmail.com>
2018-03-09 18:09:53 +0100

    Use separate Messages.sh for each Python extension

--- src/scripts/hellopython/Messages.sh
+++ src/scripts/hellopython/Messages.sh
@@ -8,4 +8,4 @@ XGETTEXT_FLAGS_PYTHON="\
 -ki18n:1 -ki18np:1,2 \
 "
 
-$XGETTEXT_PROGRAM $XGETTEXT_FLAGS_PYTHON `find hellopython -name '*.py'` -o $podir/falkon_hellopython.pot
+$XGETTEXT_PROGRAM $XGETTEXT_FLAGS_PYTHON `find . -name '*.py'` -o $podir/falkon_hellopython.pot
