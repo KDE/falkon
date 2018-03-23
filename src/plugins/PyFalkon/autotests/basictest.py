fd8cc0b356597d893a921a66a5b129f9daf02b5e
David Rosca <nowrep@gmail.com>
2018-03-23 18:05:54 +0100

    PyFalkon: Add basictest

---
+++ src/plugins/PyFalkon/autotests/basictest.py
@@ -0,0 +1,20 @@
+import Falkon
+import unittest
+
+class BasicTest(unittest.TestCase):
+
+    def test_module_version(self):
+        self.assertEqual(Falkon.__version__.count('.'), 2)
+        self.assertIsNotNone(Falkon.registerPlugin)
+
+    def test_mainapplication(self):
+        self.assertIsNotNone(Falkon.MainApplication.instance())
+
+    def test_create_window(self):
+        window = Falkon.MainApplication.instance().createWindow(Falkon.Qz.BW_NewWindow)
+        self.assertIsNotNone(window)
+
+
+suite = unittest.defaultTestLoader.loadTestsFromTestCase(BasicTest)
+if unittest.TextTestRunner().run(suite).failures:
+    raise(Exception("FAIL"))
