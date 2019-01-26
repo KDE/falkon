KDE/falkon

commit 1c4a8ddd6f8e896787c837d37e9ad596c973e436

PyFalkon: Fix build with PySide 5.12
BUG: 402947
FIXED-IN: 3.1.0

src/plugins/PyFalkon/pyfalkon_global.h
@@ -16,9 +16,6 @@
 * along with this program.  If not, see <http://www.gnu.org/licenses/>.
 * ============================================================ */
 
-// Make "signals:", "slots:" visible as access specifiers
-#define QT_ANNOTATE_ACCESS_SPECIFIER(a) __attribute__((annotate(#a)))
-
 // PYSIDE-711
 #include <QHstsPolicy>
 #include <QWebEngineContextMenuData>

src/lib/tools/desktopfile.h
@@ -26,7 +26,7 @@ class QSettings;
 class FALKON_EXPORT DesktopFile
 {
 public:
-    explicit DesktopFile();
+    DesktopFile();
     explicit DesktopFile(const QString &fileName);
 
     QString fileName() const;

commit 9a03f197caaacb171c302ff40bedf828e60e34f8

PyFalkon: Fix generating bindings for signals
Regression from 1c4a8ddd6f8e896787c837d37e9ad596c973e436

src/plugins/PyFalkon/pyfalkon_global.h
@@ -16,6 +16,9 @@
 * along with this program.  If not, see <http://www.gnu.org/licenses/>.
 * ============================================================ */
 
+// Make "signals:", "slots:" visible as access specifiers
+#define QT_ANNOTATE_ACCESS_SPECIFIER(a) __attribute__((annotate(#a)))
+
 // PYSIDE-711
 #include <QHstsPolicy>
 #include <QWebEngineContextMenuData>