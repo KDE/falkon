dc8d53280dfa7c693a090d2f7aaffb6c6c0dbda9
Juraj Oravec <jurajoravec@mailo.com>
2025-01-03 01:33:29 +0100

    AdblockDialog: Apply filter when tab changes
    Signed-off-by: Juraj Oravec <jurajoravec@mailo.com>

--- src/lib/adblock/adblockdialog.cpp
+++ src/lib/adblock/adblockdialog.cpp
@@ -126,6 +126,7 @@ void AdBlockDialog::currentChanged(int index)
     if (index != -1) {
         m_currentTreeWidget = qobject_cast<AdBlockTreeWidget*>(tabWidget->widget(index));
         m_currentSubscription = m_currentTreeWidget->subscription();
+        filterString(search->text());
     }
 }
 
